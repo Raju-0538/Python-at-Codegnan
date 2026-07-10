b167395b1cf5139d825c668346a88d650d6574be
aravind000123 <aravind.gelam@sasi.ac.in>
2026-07-11 01:06:57 +0530

    Committed

--- classes/File Handling/app.py
+++ classes/File Handling/app.py
@@ -1,23 +1,46 @@
 import csv
 import pandas as pd
+# n = int(input("enter the N value : " ))
+# fields = ['name','age','gender']
 
-fields = ['name','age','email']
-n = int(input("Enter the N value : "))
+# with open("Information.csv","w",newline="") as f:
+#     writer = csv.DictWriter(f,fieldnames=fields)
+#     writer.writeheader()
+#     for i in range(n):
+#         name = input("Enter your name :")
+#         age = int(input("Enter your age : "))
+#         gender = input("Enter your gender : ")
+#         writer.writerow({'name' : name,'age' : age,'gender' : gender})
 
-with open('students.csv','w',newline='') as f:
-    write = csv.DictWriter(f,fieldnames=fields)
-    write.writeheader()
-    for i in range(n):
-        students = {}
-        students['name'] = input("Enter name : ")
-        students['age'] = int(input("Enter age : "))
-        students['email'] = input("Enter email : ")
-        write.writerow(students)
+# with open('Information.csv','r') as f:
+#     read = csv.DictReader(f)
+#     for i in read:
+#         print(i)
 
-    print("Data added Successfully !")
-with open('students.csv','r') as file:
-    read = csv.DictReader(file)
-    for i in read:
-        print(i)
-f = pd.read_csv('Details.csv')
-print(f)
+
+fields = ['id','name','password']
+x = int(input("Enter X value : "))
+users = {}
+
+for i in range(x):
+        id = int(input("ID : "))
+        name = input("Name : ")
+        password = input("Password : ")
+        users[id] = {
+            'name' : name,
+            'password' : password
+        }
+with open("Nested.csv", "w", newline="") as f:
+    writer = csv.DictWriter(f, fieldnames=fields)
+
+    writer.writeheader()
+
+    for user_id, details in users.items():
+        writer.writerow({
+            "id": user_id,
+            "name": details["name"],
+            "password": details["password"]
+        })
+data = pd.read_csv('Nested.csv')
+df = pd.DataFrame(data)
+print(df)
