d791b9c6afb44b7aa7f7f45027550409f525b8d3
aravind000123 <aravind.gelam@sasi.ac.in>
2026-07-15 22:18:02 +0530

    15-07-26

--- classes/File Handling/Online shopping cart/removestock.py
+++ classes/File Handling/Online shopping cart/removestock.py
@@ -1,2 +1,47 @@
-def removestock():
-    pass
+import csv
+import os
+
+
+def removestock(name: str, quantity: int):
+
+    products = {}
+
+    if os.path.exists("Products.csv"):
+
+        with open("Products.csv", "r", newline="") as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                products[row["product"]] = {
+                    "stock": int(row["stock"]),
+                    "price": int(row["price"])
+                }
+
+    else:
+        return "Products.csv does not exist!"
+
+    if name not in products:
+        return "Product Not Found!"
+
+    if quantity > products[name]["stock"]:
+        return "Quantity exceeds the Stock !"
+
+    products[name]["stock"] -= quantity
+
+    fields = ["product", "stock", "price"]
+
+    with open("Products.csv", "w", newline="") as f:
+
+        writer = csv.DictWriter(f, fieldnames=fields)
+
+        writer.writeheader()
+
+        for product, details in products.items():
+
+            writer.writerow({
+                "product": product,
+                "stock": details["stock"],
+                "price": details["price"]
+            })
+
+    return "Stock Updated Successfully!"
